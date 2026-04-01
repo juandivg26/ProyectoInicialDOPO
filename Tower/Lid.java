b25c69e8b3f7cd3715fbc09d14ade5466e51cbb2
Juan Diego Valderrama Gaviria <juan.valderrama-g@mail.escuelaing.edu.co>
2026-04-01 02:14:16 -0500

    Refactor Lid class to be abstract and adjust methods
    Updated Lid class to be abstract and modified method access levels.

--- Tower/Lid.java
+++ Tower/Lid.java
@@ -1,16 +1,29 @@
+package tower;
+
+import shapes.Rectangle;
+
 /**
  * Una tapa de la torre. Es un rectangulo solido.
- * La altura de cualquier tapa es 1 cm.
+ * La altura de cualquier tapa es 1 cm (excepto LidCrazy que es diferente).
  * Hereda de StackItem.
+ * 
+ * Clase ABSTRACTA que sirve como base para tipos específicos de tapas:
+ * - LidNormal: comportamiento estándar, altura = 1
+ * - LidFearful: no sale si está tapando su taza
+ * - LidCrazy: se pone de base, altura = 0
  * 
  * @author Juan Diego Valderrama Gaviria y Jhonatan Madero
  * @version 3.0
  */
-public class Lid extends StackItem {
+public abstract class Lid extends StackItem {
+
+    protected static final int SCALE = 15;
+    protected static final int DEFAULT_X = 70;
+    protected static final int DEFAULT_Y = 15;
 
     /**
      * Crea una tapa para la torre.
-     * La altura siempre es 1 cm.
+     * La altura por defecto es 1 cm (pero LidCrazy lo sobrescribe).
      * @param number numero de la tapa
      * @param color color de la tapa
      */
@@ -23,7 +36,7 @@ public class Lid extends StackItem {
      * Crea el rectangulo que forma la tapa.
      * La tapa es un solo rectangulo solido.
      */
-    private void createShape() {
+    protected void createShape() {
         int widthPx = number * SCALE;
         
         base = new Rectangle();
@@ -37,18 +50,25 @@ public class Lid extends StackItem {
      * Retorna el tipo de este elemento.
      * @return "lid"
      */
+    @Override
     public String getType() {
         return "lid";
     }
     
     /**
+     * Retorna el subtipo específico de tapa
+     * Cada subclase implementa esto
+     * @return "normal", "fearful", "crazy", etc.
+     */
+    public abstract String getSubtype();
+    
+    /**
      * Dibuja la tapa en la posicion indicada dentro de la torre.
-     * Posiciona el rectangulo solido en la posicion vertical correcta.
-     * Implementa el metodo abstracto del padre.
      * @param towerCenterX centro horizontal de la torre en pixeles
      * @param towerBaseY posicion Y de la base de la torre en pixeles
      * @param yPositionCm posicion Y del elemento en cm desde la base
      */
+    @Override
     public void draw(int towerCenterX, int towerBaseY, int yPositionCm) {
         int widthPx = number * SCALE;
         int xLeft = towerCenterX - widthPx / 2;
@@ -66,8 +86,8 @@ public class Lid extends StackItem {
     /**
      * Hace invisible la tapa.
      * Oculta el rectangulo de la tapa.
-     * Implementa el metodo abstracto del padre.
      */
+    @Override
     public void erase() {
         base.makeInvisible();
         drawn = false;
